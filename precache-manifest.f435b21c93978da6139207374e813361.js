1ace5b26bab06e975d8dfa782463d2a942a41a6d
Anton Finnson <finnsonanton@gmail.com>
2022-05-27 16:59:04 +0200

    Updates

---
+++ precache-manifest.f435b21c93978da6139207374e813361.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "78e4b2287fe303074da2",
+    "url": "/tb-app/static/css/main.c1177ad4.chunk.css"
+  },
+  {
+    "revision": "78e4b2287fe303074da2",
+    "url": "/tb-app/static/js/main.78e4b228.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/tb-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "2ded161d425ce65a7129",
+    "url": "/tb-app/static/js/2.2ded161d.chunk.js"
+  },
+  {
+    "revision": "a284edcec31823337926",
+    "url": "/tb-app/static/js/runtime~main.a284edce.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/tb-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9e23fb78cdcc4c244b3af6fb47b46fba",
+    "url": "/tb-app/index.html"
+  }
+];
